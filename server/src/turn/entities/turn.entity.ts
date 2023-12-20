1916b148fd0bd37e09d47bc7b96c264c239c5b03
nicolas-cardona <francisco@wecraftapps.com>
2023-12-20 20:42:24 +0100

    new entity for resource Turn

--- server/src/turn/entities/turn.entity.ts
+++ server/src/turn/entities/turn.entity.ts
@@ -1,1 +1,39 @@
-export class Turn {}
+import { IsNotEmpty, IsUUID, IsDateString, IsInt } from 'class-validator';
+import { UUID } from 'crypto';
+export class Turn {
+  @IsNotEmpty()
+  @IsUUID()
+  turn_id: UUID;
+
+  @IsNotEmpty()
+  @IsUUID()
+  match_id: UUID;
+
+  @IsNotEmpty()
+  @IsInt()
+  integer_1: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  integer_2: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  integer_3: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  turn_order: number;
+
+  @IsNotEmpty()
+  @IsDateString()
+  created_at: Date;
+
+  @IsNotEmpty()
+  @IsDateString()
+  updated_at: Date;
+
+  @IsDateString()
+  deleted_at: Date;
+
+}
