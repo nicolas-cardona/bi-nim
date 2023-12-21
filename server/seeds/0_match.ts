14dab17d62adfa54bc906157413322d5c06d16ec
nicolas-cardona <francisco@wecraftapps.com>
2023-12-21 16:25:13 +0100

    changes on seeds following the changes on tables match and turn

--- server/seeds/0_match.ts
+++ server/seeds/0_match.ts
@@ -10,30 +10,19 @@ export async function seed(knex: Knex): Promise<void> {
   await knex('nim.match').insert([
     {
       match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
-      integer_1: 1,
-      integer_2: 1,
-      // integer_3: ,
       first_player: 'COMPUTER',
       winner: 'USER',
       match_finished: true,
     },
     {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
-      integer_1: 3,
-      integer_2: 2,
-      integer_3: 1,
       first_player: 'USER',
       winner: 'COMPUTER',
       match_finished: true,
     },
     {
       match_id: '6b0fc914-4e24-49b5-a6e9-d4516e15e502',
-      integer_1: 4,
-      integer_2: 3,
-      integer_3: 2,
       first_player: 'COMPUTER',
-      // winner: ,
-      match_finished: false,
     },
   ]);
 }
--- server/seeds/1_turn.ts
+++ server/seeds/1_turn.ts
@@ -9,40 +9,54 @@ export async function seed(knex: Knex): Promise<void> {
   // Inserts seed entries
   await knex('nim.turn').insert([
     {
-      // turn_id:
+      match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
+      integer_1: 1,
+      integer_2: 1,
+      turn_order: 0,
+    },
+    {
       match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
       integer_1: 1,
       turn_order: 1,
     },
     {
-      // turn_id:
       match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
       turn_order: 2,
     },
     {
-      // turn_id:
+      match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
+      integer_1: 3,
+      integer_2: 2,
+      integer_3: 1,
+      turn_order: 0,
+    },
+    {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
       integer_1: 3,
       integer_2: 2,
       turn_order: 1,
     },
     {
-      // turn_id:
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
       integer_1: 2,
       integer_2: 2,
       turn_order: 2,
     },
     {
-      // turn_id:
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
       integer_1: 2,
       turn_order: 3,
     },
     {
-      // turn_id:
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
       turn_order: 4,
+    },
+    {
+      match_id: '6b0fc914-4e24-49b5-a6e9-d4516e15e502',
+      integer_1: 4,
+      integer_2: 3,
+      integer_3: 2,
+      turn_order: 0,
     },
   ]);
 }
