c7a1b131a5d3eea7be25055aca2b4177f90234e2
nicolas-cardona <francisco@wecraftapps.com>
2023-12-21 16:42:44 +0100

    first version of TurnController

--- server/src/turn/turn.controller.ts
+++ server/src/turn/turn.controller.ts
@@ -1,7 +1,23 @@
-import { Controller, Get, Post, Body, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { TurnService } from './turn.service';
+import { Turn } from './entities/turn.entity';
+import { UUID } from 'crypto';
+import { TurnPlayed } from './dto/turn-played.dto';
 
 @Controller('turn')
 export class TurnController {
   constructor(private readonly turnService: TurnService) {}
+
+  @Post('new')
+  async newMatch(@Body() turnPlayed: TurnPlayed): Promise<Turn> {
+    const turn = await this.turnService.createTurn(turnPlayed);
+    return await this.turnService.add(turn);
+  }
+
+  @Get(':matchId')
+  async getOne(@Param('matchId') matchId: UUID): Promise<Turn[]> {
+    return await this.turnService.find({
+      match_id: matchId,
+    });
+  }
 }
