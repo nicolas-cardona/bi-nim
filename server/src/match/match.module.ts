fac12aec78aa1fa1707064d4e2e6bcaf90752cd9
nicolas-cardona <francisco@wecraftapps.com>
2023-12-21 16:33:45 +0100

    import of TurnModule in MatchModule

--- server/src/match/match.module.ts
+++ server/src/match/match.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
 import { MatchModel } from './match.model';
+import { TurnModule } from 'src/turn/turn.module';
 
 @Module({
+  imports: [TurnModule],
   controllers: [MatchController],
   providers: [MatchService, MatchModel],
   exports: [MatchService],
