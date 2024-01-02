001d30584a1bac0fc4a1d56c440a80f03c8c198f
nicolas-cardona <francisco@wecraftapps.com>
2024-01-02 20:28:31 +0100

    import of StrategyModule in TurnModule

--- server/src/turn/turn.module.ts
+++ server/src/turn/turn.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TurnService } from './turn.service';
 import { TurnController } from './turn.controller';
 import { TurnModel } from './turn.model';
+import { StrategyModule } from 'src/strategy/strategy.module';
 
 @Module({
+  imports: [StrategyModule],
   controllers: [TurnController],
   providers: [TurnService, TurnModel],
   exports: [TurnService],
