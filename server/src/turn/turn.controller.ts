e367cc7f0e5a18d08f9605969962961f2d9b421a
nicolas-cardona <francisco@wecraftapps.com>
2024-01-03 17:55:22 +0100

    in TurnController: use of matchUnfinishedVerification function

--- server/src/turn/turn.controller.ts
+++ server/src/turn/turn.controller.ts
@@ -4,16 +4,23 @@ import { Turn } from './entities/turn.entity';
 import { UUID } from 'crypto';
 import { TurnPlayed } from './dto/turn-played.dto';
 import { StrategySelected } from './dto/strategy-selected.dto';
+import { MatchService } from 'src/match/match.service';
 
 @Controller('turn')
 export class TurnController {
-  constructor(private readonly turnService: TurnService) {}
+  constructor(
+    private readonly turnService: TurnService,
+    private readonly matchService: MatchService,
+  ) {}
 
   @Post(':matchId/user')
   async newUserMatch(
     @Param('matchId') matchId: UUID,
     @Body() turnPlayed: TurnPlayed,
   ): Promise<Turn> {
+    await this.matchService.matchUnfinishedVerification({
+      match_id: matchId,
+    });
     const lastTurnPosted = await this.turnService.findLastOne({
       match_id: matchId,
     });
@@ -26,6 +33,9 @@ export class TurnController {
     @Param('matchId') matchId: UUID,
     @Body() strategySelected: StrategySelected,
   ): Promise<Turn> {
+    await this.matchService.matchUnfinishedVerification({
+      match_id: matchId,
+    });
     const lastTurnPosted = await this.turnService.findLastOne({
       match_id: matchId,
     });
