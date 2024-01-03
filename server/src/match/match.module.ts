879c01a01d535354b17d182797d8d87955fe05da
nicolas-cardona <francisco@wecraftapps.com>
2024-01-03 17:32:30 +0100

    use of forwardRef to avoid circular dependencies

--- server/src/match/match.module.ts
+++ server/src/match/match.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MatchService } from './match.service';
 import { MatchController } from './match.controller';
 import { MatchModel } from './match.model';
@@ -6,7 +6,7 @@ import { TurnModule } from 'src/turn/turn.module';
 import { StrategyModule } from 'src/strategy/strategy.module';
 
 @Module({
-  imports: [TurnModule, StrategyModule],
+  imports: [forwardRef(() => TurnModule), StrategyModule],
   controllers: [MatchController],
   providers: [MatchService, MatchModel],
   exports: [MatchService],
