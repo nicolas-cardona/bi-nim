35e413df6e60bda3b2ff1a8ce56b108a681077c7
nicolas-cardona <francisco@wecraftapps.com>
2024-01-03 15:57:30 +0100

    change of route for posting a new user turn. New route to pôst a new computer turn

--- server/src/turn/turn.controller.ts
+++ server/src/turn/turn.controller.ts
@@ -3,14 +3,37 @@ import { TurnService } from './turn.service';
 import { Turn } from './entities/turn.entity';
 import { UUID } from 'crypto';
 import { TurnPlayed } from './dto/turn-played.dto';
+import { StrategySelected } from './dto/strategy-selected.dto';
 
 @Controller('turn')
 export class TurnController {
   constructor(private readonly turnService: TurnService) {}
 
-  @Post('new')
-  async newMatch(@Body() turnPlayed: TurnPlayed): Promise<Turn> {
-    const turn = await this.turnService.createTurn(turnPlayed);
+  @Post(':matchId/user')
+  async newUserMatch(
+    @Param('matchId') matchId: UUID,
+    @Body() turnPlayed: TurnPlayed,
+  ): Promise<Turn> {
+    const lastTurnPosted = await this.turnService.findLastOne({
+      match_id: matchId,
+    });
+    const turn = await this.turnService.createTurn(turnPlayed, lastTurnPosted);
+    return await this.turnService.add(turn);
+  }
+
+  @Post(':matchId/computer')
+  async newComputerMatch(
+    @Param('matchId') matchId: UUID,
+    @Body() strategySelected: StrategySelected,
+  ): Promise<Turn> {
+    const lastTurnPosted = await this.turnService.findLastOne({
+      match_id: matchId,
+    });
+    const turnPlayed = await this.turnService.createComputerTurnPlayed(
+      lastTurnPosted,
+      strategySelected,
+    );
+    const turn = await this.turnService.createTurn(turnPlayed, lastTurnPosted);
     return await this.turnService.add(turn);
   }
 
