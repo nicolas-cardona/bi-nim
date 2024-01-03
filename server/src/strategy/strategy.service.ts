3285b1affb91ef41390430759a246bafecb11616
nicolas-cardona <francisco@wecraftapps.com>
2024-01-03 15:39:46 +0100

    change on functions name: selectionWithStrategy -> selectionWinningStrategy. Minor changes on functions selectionRandomStrategy and binarySetsEqualizer

--- server/src/strategy/strategy.service.ts
+++ server/src/strategy/strategy.service.ts
@@ -5,10 +5,10 @@ export class StrategyService {
 
   // Function that returns a list with two numbers. The first one is
   // a number contained in the array of the entry and the second one
-  // is the value that should replace the first one to gurantee the
-  // victory. If this is not possible, it returns null.
-  public selectionWithStrategy(array: number[]): number[] {
-    const arrayCopy = [...array];
+  // is the value that should substracted to the first one to gurantee
+  // the victory. If this is not possible, it returns null.
+  public selectionWinningStrategy(integersArray: number[]): number[] {
+    const arrayCopy = [...integersArray];
     const arraySorted = arrayCopy.sort((a: number, b: number) => a - b);
     let answer = null;
 
@@ -28,17 +28,21 @@ export class StrategyService {
 
   // Function that returns a list with two numbers. The first one is
   // a number contained in the array of the entry and the second one
-  // is a randoml value that can replace the first one.
-  public selectionRandom(array: number[]): number[] {
-    const arrayCopy = [...array];
+  // is a random value that can be substracted to the first one.
+  public selectionRandomStrategy(integersArray: number[]): number[] {
+    const arrayCopy = [...integersArray];
     const arraySorted = arrayCopy.sort((a: number, b: number) => a - b);
-    arraySorted[0] = 0;
+    const arraySortedWithZero = [0].concat(arraySorted);
     let answer = null;
 
-    for (let i = 2; i >= 0; i--) {
-      if (arraySorted[i - 1] < arraySorted[i]) {
-        const integer = this.getRandomInt(arraySorted[i - 1], arraySorted[i]);
-        answer = [arraySorted[i], integer];
+    for (let i = 3; i > 0; i--) {
+      if (arraySortedWithZero[i - 1] < arraySortedWithZero[i]) {
+        const integer = this.getRandomInt(
+          1,
+          arraySortedWithZero[i] - arraySortedWithZero[i - 1],
+        );
+
+        answer = [arraySortedWithZero[i], integer];
         break;
       }
     }
@@ -46,10 +50,10 @@ export class StrategyService {
   }
 
   // Given 3 non-negative integers x, y and z this function returns
-  // a strictly lesser integer than z in such a way that its binary
-  // set (a set obtained by using the function getBinarySet) is equal
-  // to the symetric difference of the binary sets of x and y.
-  // If this is not possiible it returns -1.
+  // the integer that should be substracted from z in order that the
+  // binary set (a set obtained by using the function getBinarySet)
+  // of this difference is equal to the symetric difference of the
+  // binary sets of x and y. If this is not possiible it returns -1.
   private binarySetsEqualizer(x: number, y: number, z: number): number {
     if (x < 0 || y < 0 || z < 0) {
       throw new Error('integers must be non-negative');
@@ -59,7 +63,7 @@ export class StrategyService {
     const delta = this.symmetricDifference(xBinarySet, yBinarySet);
     const deltaNumeric = this.getNumericExpression(delta);
     if (deltaNumeric < z) {
-      return deltaNumeric;
+      return z - deltaNumeric;
     } else {
       return -1;
     }
@@ -125,10 +129,10 @@ export class StrategyService {
   // Function that generates a random integer in the interval [minimum, supremum)
   // If minimum = supremum, it returns this value
   private getRandomInt(minimum: number, supremum: number) {
-    if (minimum < supremum) {
+    if (minimum <= supremum) {
       return Math.floor(Math.random() * (supremum - minimum) + minimum);
     } else {
-      throw new Error('supremumm must be greater than minimum');
+      throw new Error('supremumm must be greater or equal than minimum');
     }
   }
 }
