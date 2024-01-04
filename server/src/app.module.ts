935d322c473f775f851306b95ae3a64b4a6dfd13
nicolas-cardona <francisco@wecraftapps.com>
2024-01-04 15:37:48 +0100

    new resource Statistics

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MatchModule } from './match/match.module';
 import { TurnModule } from './turn/turn.module';
 import { StrategyModule } from './strategy/strategy.module';
+import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [MatchModule, TurnModule, StrategyModule],
+  imports: [MatchModule, TurnModule, StrategyModule, StatisticsModule],
   controllers: [AppController],
   providers: [AppService],
 })
