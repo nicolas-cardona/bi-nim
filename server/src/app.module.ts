4a309920ec63d3f344efc77a388960a211070866
nicolas-cardona <francisco@wecraftapps.com>
2024-01-08 12:21:20 +0100

    defaul file app.service and app.controller deleted

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MatchModule } from './match/match.module';
 import { TurnModule } from './turn/turn.module';
-import { StrategyModule } from './strategy/strategy.module';
-import { StatisticsModule } from './statistics/statistics.module';
 
 @Module({
-  imports: [MatchModule, TurnModule, StrategyModule, StatisticsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MatchModule, TurnModule],
 })
 export class AppModule {}
