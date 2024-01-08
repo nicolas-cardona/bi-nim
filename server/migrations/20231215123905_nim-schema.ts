61ad7f7c3184b0cf2b957d5f00406e71840ed7f5
nicolas-cardona <francisco@wecraftapps.com>
2024-01-08 17:46:01 +0100

    trigger for updating deleted

--- server/migrations/20231215123905_nim-schema.ts
+++ server/migrations/20231215123905_nim-schema.ts
@@ -2,15 +2,6 @@ import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.raw(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`);
-  // Function to update automatically column updated_at when updating a row of a table
-  await knex.schema.raw(`
-    CREATE OR REPLACE FUNCTION set_updated_at() RETURNS TRIGGER AS $$
-    BEGIN
-      NEW.updated_at = NOW();
-      RETURN NEW;
-    END;
-    $$ LANGUAGE plpgsql;
-  `);
   await knex.schema.createSchemaIfNotExists('nim');
 }
 
