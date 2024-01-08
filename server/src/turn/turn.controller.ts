3f9e710bf7bc836eafb422111bfc2fb67c95beb1
nicolas-cardona <francisco@wecraftapps.com>
2024-01-08 17:47:47 +0100

    use of ParseUUIDPipe in TurnController

--- server/src/turn/turn.controller.ts
+++ server/src/turn/turn.controller.ts
@@ -1,4 +1,11 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { TurnService } from './turn.service';
 import { Turn } from './entities/turn.entity';
 import { UUID } from 'crypto';
@@ -14,7 +21,7 @@ export class TurnController {
 
   @Post(':matchId/user')
   async newUserMatch(
-    @Param('matchId') matchId: UUID,
+    @Param('matchId', ParseUUIDPipe) matchId: UUID,
     @Body() turnPlayed: TurnPlayed,
   ): Promise<Turn> {
     await this.matchService.matchUnfinishedVerification({
@@ -30,7 +37,9 @@ export class TurnController {
   }
 
   @Get(':matchId')
-  async getOne(@Param('matchId') matchId: UUID): Promise<Turn[]> {
+  async getOne(
+    @Param('matchId', ParseUUIDPipe) matchId: UUID,
+  ): Promise<Turn[]> {
     return await this.turnService.find({
       match_id: matchId,
     });
