514a218683536219d24182a02687196f1bab1510
nicolas-cardona <francisco@wecraftapps.com>
2024-01-09 13:37:39 +0100

    In MatchController: route POST /matches/ uses function addWithTurnTransaction instead of add

--- server/src/match/match.controller.ts
+++ server/src/match/match.controller.ts
@@ -10,14 +10,17 @@ import { MatchService } from './match.service';
 import { Match } from './entities/match.entity';
 import { MatchOptionsDto } from './dto/match-options.dto';
 import { UUID } from 'crypto';
+import { Turn } from 'src/turn/entities/turn.entity';
 
 @Controller('matches')
 export class MatchController {
   constructor(private readonly matchService: MatchService) {}
 
   @Post('/')
-  async newMatch(@Body() matchOptionsDto: MatchOptionsDto): Promise<Match> {
-    return await this.matchService.add(matchOptionsDto);
+  async newMatch(
+    @Body() matchOptionsDto: MatchOptionsDto,
+  ): Promise<[Match, Turn]> {
+    return await this.matchService.addWithTurnTransaction(matchOptionsDto);
   }
 
   @Get(':matchId')
