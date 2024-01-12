fa0f0ee97666e8a7c72cddbd4e76ea4836616b75
nicolas-cardona <francisco@wecraftapps.com>
2024-01-12 19:32:03 +0100

    seeds changed following changes on tables

--- server/seeds/0_match.ts
+++ server/seeds/0_match.ts
@@ -10,19 +10,16 @@ export async function seed(knex: Knex): Promise<void> {
   await knex('nim.match').insert([
     {
       match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
-      first_player: 'COMPUTER',
       winner: 'USER',
       match_finished: knex.fn.now(),
     },
     {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
-      first_player: 'USER',
       winner: 'COMPUTER',
       match_finished: knex.fn.now(),
     },
     {
       match_id: '6b0fc914-4e24-49b5-a6e9-d4516e15e502',
-      first_player: 'COMPUTER',
     },
   ]);
 }
--- server/seeds/1_turn.ts
+++ server/seeds/1_turn.ts
@@ -10,21 +10,25 @@ export async function seed(knex: Knex): Promise<void> {
   await knex('nim.turn').insert([
     {
       match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
+      next_player: 'COMPUTER',
       integer_1: 1,
       integer_2: 1,
       turn_order: 0,
     },
     {
       match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
+      next_player: 'USER',
       integer_1: 1,
       turn_order: 1,
     },
     {
       match_id: 'a9392766-2e7d-4b8a-87e8-ab9b0dc25a29',
+      next_player: 'COMPUTER',
       turn_order: 2,
     },
     {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
+      next_player: 'USER',
       integer_1: 3,
       integer_2: 2,
       integer_3: 1,
@@ -32,27 +36,32 @@ export async function seed(knex: Knex): Promise<void> {
     },
     {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
+      next_player: 'COMPUTER',
       integer_1: 3,
       integer_2: 2,
       turn_order: 1,
     },
     {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
+      next_player: 'USER',
       integer_1: 2,
       integer_2: 2,
       turn_order: 2,
     },
     {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
+      next_player: 'COMPUTER',
       integer_1: 2,
       turn_order: 3,
     },
     {
       match_id: 'dadbd149-5ca8-4b72-b12c-84572d145670',
+      next_player: 'USER',
       turn_order: 4,
     },
     {
       match_id: '6b0fc914-4e24-49b5-a6e9-d4516e15e502',
+      next_player: 'COMPUTER',
       integer_1: 4,
       integer_2: 3,
       integer_3: 2,
