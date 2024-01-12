8a1e98823f3875b1ca0b55ca48d056160293560e
nicolas-cardona <francisco@wecraftapps.com>
2024-01-12 19:30:44 +0100

    In table Match: column first_player is deleted

--- server/migrations/20231215134428_match.ts
+++ server/migrations/20231215134428_match.ts
@@ -8,7 +8,6 @@ export async function up(knex: Knex): Promise<void> {
   await knex.schema.raw(`
     CREATE TABLE IF NOT EXISTS nim.match(
       match_id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-      first_player player NOT NULL,
       winner player,
       match_finished TIMESTAMPTZ(3),
       created_at TIMESTAMPTZ(3) DEFAULT NOW()
